2173da976406efe1d01a1eba29f35795ad0a9be2
quinnmckay <quinnmckay83@gmail.com>
2024-01-24 13:35:58 -0800

    Completed Java Lab 002

--- src/StumpTheChump.java
+++ src/StumpTheChump.java
@@ -1,6 +1,17 @@
 public class StumpTheChump {
     // Initially working program.
     public static void main(String[] args) {
+
         System.out.println("Hi Chump, can you be stumped?");
+
+        int day = 24;
+        int month = 1;
+        int year = 2024;
+        String date =("Date: " + month + "/" + day + "/" + year);
+
+        System.out.println("Day: " + day);
+        System.out.println("Month: " + month);
+        System.out.println("Year: " + year);
+        System.out.println(date);
     }
 }
