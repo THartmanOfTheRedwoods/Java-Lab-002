ce2e49a9228b671c04afecaccfe5cf8e34534827
Eliot Rodriguez <erodriguez314@mycr.redwoods.edu>
2024-01-23 19:47:54 -0800

    Java Lab 002 complete

---
+++ src/Date.java
@@ -0,0 +1,25 @@
+public class Date {
+    static String day = "Tuesday";
+    static String month = "January";
+    static int date = 23;
+    static int year = 2024;
+
+    public static void main(String[] args) {
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.print(year);
+        System.out.println();
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.print(", ");
+        System.out.print(year);
+    }
+
+}
