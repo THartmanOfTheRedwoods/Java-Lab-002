61f62cb08a4361a5515a8b482583278e493315bd
hufflpuffz <chaseburtonschool@gmail.com>
2024-01-24 13:29:17 -0800

    I wrote a program to show the current date and made an error for debugging for lab 002

--- src/StumpTheChump.java
+++ src/StumpTheChump.java
@@ -2,5 +2,15 @@ public class StumpTheChump {
     // Initially working program.
     public static void main(String[] args) {
         System.out.println("Hi Chump, can you be stumped?");
+
+            int day= 24;
+            String date;
+            int month= 1;
+            int year= 2024;
+            System.out.println(day+"\n"+month+"\n"+year);
+            date = day+"-"+month+"-"+year;
+            System.out.println(date);
+            date = month+"-"+day+"-"+year;
+            System.out.println(date);
     }
 }
