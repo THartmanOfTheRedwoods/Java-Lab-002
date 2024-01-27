bbfe44c560a1f0802381be52e744225d096182d2
John Dennis <jdennis432@gmail.com>
2024-01-27 14:24:40 -0800

    John Dennis

--- src/StumpTheChump.java
+++ src/StumpTheChump.java
@@ -2,5 +2,12 @@ public class StumpTheChump {
     // Initially working program.
     public static void main(String[] args) {
         System.out.println("Hi Chump, can you be stumped?");
+        int hour = 5;
+        int minute = 55;
+        System.out.print("the current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.print(minute);
+        System.out.println(".");
     }
 }
---
+++ src/date.java
@@ -0,0 +1,22 @@
+public class date {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String day = "Thursday";
+        int date = 26;
+        String month = "January";
+        int year = 2024;
+        System.out.print("Today's date it ");
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.print(year);
+        System.out.println(".");
+        System.out.print(date);
+        System.out.print("/");
+        System.out.print(month);
+        System.out.print("/");
+        System.out.print(year);}
+}
