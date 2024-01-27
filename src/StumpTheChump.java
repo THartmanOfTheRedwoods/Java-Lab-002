a3a91a3b8c157b05cec218cba1187e1c5e2483e7
Shuulker <JosephSparrowRoberts26@gmail.com>
2024-01-26 21:03:28 -0800

    Completed Java-Lab-002

--- src/StumpTheChump.java
+++ src/StumpTheChump.java
@@ -5,14 +5,14 @@ public class StumpTheChump {
     public static void main(String[] args) {
         System.out.println("Hi Chump, can you be stumped?");
         System.out.println("These are the chumps...");
-        ArrayList<String> chumps = new ArrayList<String>();
+        ArrayList<String> chumps = new ArrayList<>();
         chumps.add("Joseph");
         chumps.add("William");
         chumps.add("Sebastian");
         chumps.add("Ibrahim");
 
         for (int i = 0; i < chumps.size(); i++)
-        System.out.print(chumps.get(i));
+        System.out.println(chumps.get(i));
 
     }
 }
