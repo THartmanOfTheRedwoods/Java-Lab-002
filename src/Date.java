a12bb58475663a8e8e7ab5eebe77eeafa2ba0b48
jli667 <JennyL137740@gmail.com>
2024-01-27 22:53:37 -0800

    Completed lab 002.

---
+++ src/Date.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author Jenny Li
+ * @since Version 1.0
+ * (1/27/24)
+ *
+ */
+
+public class Date {
+    public static void main(String[] args) {
+        int day;
+        day = 27;
+        String date;
+        date = "Saturday";
+        String month;
+        month = "January";
+        int year;
+        year = 2024;
+
+        //System.out.println(date + "," + month + "," + day + "," + year);
+        System.out.println(date + "," + day + "," + month + "," + year);
+    }
+}
