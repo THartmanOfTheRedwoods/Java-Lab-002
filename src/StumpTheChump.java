e46e86425d751aa37e4e98303fb1f86ea937b0a8
BFristoe <brendanfristoe@gmail.com>
2024-01-27 17:23:16 -0800

    Edited StumpTheChump for challenges with partner Created Date program Edited README

--- src/StumpTheChump.java
+++ src/StumpTheChump.java
@@ -2,7 +2,7 @@ public class StumpTheChump {
     // Initially working program.
     // Make this say that you can't be stumped so easy.
     public static void main(String[] args) {
-        system.out.println("Hi Chump, can you be stumped?");
+        System.out.println("Hi Chump, can you be stumped?");
         String x;
                 // x = "I have stumped the Chump.";
                 x = "You cant stump me!";
