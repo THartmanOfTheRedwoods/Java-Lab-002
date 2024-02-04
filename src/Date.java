757624c126805d361c8bca1f5776c472353ccee1
diego <Diego.car7423@gmail.com>
2024-02-03 20:17:48 -0800

    I finished the "StumpTheChump" challenge and created a program to display both American and European Standard Date Format.

---
+++ src/Date.java
@@ -0,0 +1,10 @@
+public class Date {
+    public static void main(String[] args) {
+        String month = "July";
+        String day = "Thrusday";
+        String date = "18";
+        String year = "2019";
+        System.out.println(day + "," + " " + month + " " + date + "," + year);
+        System.out.println(day + "," + " " + date + " " + month + "," + year);
+    }
+}
